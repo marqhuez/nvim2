d9252997cfcd578b3c4951deb241563caac9a8a4
Soltész Márk <mark.soltesz@shoprenter.hu>
2024-05-21 22:34:33 +0200

    gitsigns

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,7 +4,9 @@ vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.swapfile = false
 vim.opt.scrolloff = 10
+
 vim.wo.number = true
+vim.o.hlsearch = false
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
