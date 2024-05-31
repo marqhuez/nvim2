be805f65d759ff76c4c2ab81789be89745916b0b
Soltész Márk <mark.soltesz@shoprenter.hu>
2024-05-31 12:28:51 +0200

    telescope live grep special chars

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,13 @@ return {
 		tag = "0.1.6",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			local conf = require("telescope.config").values
 			local builtin = require("telescope.builtin")
+
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fw", builtin.live_grep, {
+				vimgrep_arguments = table.insert(conf.vimgrep_arguments, "--fixed-strings"),
+			})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
